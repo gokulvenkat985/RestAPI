f80c576c0b4b7d715a22e243a23160292f63415d
Gokul <gokul@ikomet.in>
2020-04-07 19:38:43 +0530

    check the get request in dbapp/Model/Review.php

--- app/Model/product.php
+++ app/Model/product.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+    public function review()
+    {
+    	return $this->hasMany(Review::class,'id');
+    	// return $this->hasMany(Chapter::class, 'module_id');
+    }
 }
