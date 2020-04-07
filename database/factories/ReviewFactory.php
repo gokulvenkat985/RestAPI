0e10b441d3d1ee153dcc37c5cfc55ec7970eecd1
Gokul <gokul@ikomet.in>
2020-04-07 17:56:36 +0530

    seeding into database

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,12 +1,18 @@
+
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-
+use App\Model\product;
 use App\Model\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=>function(){
+        	return product::all()->random();
+        },
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,5)
     ];
 });
