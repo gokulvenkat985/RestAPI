bf5852392e18a9030e385e13d04a65dc53b7563a
Gokul <gokul@ikomet.in>
2020-04-08 19:38:59 +0530

    validation

--- app/Http/Controllers/Country/Countrycontroller.php
+++ app/Http/Controllers/Country/Countrycontroller.php
@@ -14,17 +14,30 @@ class Countrycontroller extends Controller
  		// return DB::table('_z_country')->get();
  	}
  	public function getid($id){
- 		return response()->json(CountryModel::find($id),200);
+ 		$check=CountryModel::find($id);
+ 		if(is_null($check)){
+ 			return "record not found";
+ 		}
+ 		else{
+ 			return response()->json(CountryModel::find($id),200);
+ 		}
+ 		
  	}
  	public function insert(Request $request){
  		$country=CountryModel::create($request->all());
  		return response()->json($country,201);
  	}
- 	public function insertrec(Request $request,$country){    
+ 	public function insertrec(Request $request,$country){
 		$user = CountryModel::find($country);
+		if(is_null($user)){
+		return "record not found";
+		}
+		else{
 		$user->update($request->all());
 		$user = CountryModel::find($country);
-		return response()->json($user); 		 
+		return response()->json($user);
+		}
+		 		 
  	}
  	public function deleterec(Request $request,$deletecode){
  		$res=CountryModel::where('id',$deletecode)->delete();
